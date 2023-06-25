728357861095a528f2458a5b75f985f50c79cb79
Jason Reilly <jdr0887@gmail.com>
2023-06-25 10:02:00 -0400

    initial commit

---
+++ main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export $(cat .env | grep -v '^#' | xargs)
+
+export DATABASE_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER/$POSTGRES_DB"
+
+cargo install diesel_cli
+
+diesel setup
+diesel migration run --locked-schema
+
+cargo run --release
