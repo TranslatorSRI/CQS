3cda4d5cb135fe74eac8f78dc933ddaafc91d013
Jason Reilly <jdr0887@gmail.com>
2023-12-01 12:38:31 -0500

    adding cargo update to main

--- main.sh
+++ main.sh
@@ -4,6 +4,7 @@ export $(cat .env | grep -v '^#' | xargs)
 
 export DATABASE_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER/$POSTGRES_DB"
 
+cargo update
 cargo install diesel_cli
 
 diesel setup
