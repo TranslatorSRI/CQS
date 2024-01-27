57bc4cbaaf14c31fcf754a51de36950b07834377
Jason Reilly <jdr0887@gmail.com>
2024-01-27 14:16:45 -0500

    removing cargo update

--- main.sh
+++ main.sh
@@ -4,10 +4,11 @@ export $(cat .env | grep -v '^#' | xargs)
 
 export DATABASE_URL="postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_SERVER/$POSTGRES_DB"
 
-cargo update
+#cargo install -j 4 diesel_cli
 cargo install diesel_cli
 
 diesel setup
 diesel migration run --locked-schema
 
+#cargo run -j 4 --release
 cargo run --release
